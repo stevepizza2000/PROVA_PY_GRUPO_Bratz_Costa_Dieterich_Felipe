cb57a6140deba502ee81280a126fe94ed2fdce1e
stevepizza2000 <arthurbratz1@gmail.com>
2025-11-18 16:50:48 -0300

    Update gui_app.py

--- PROVA_PY_GRUPO_Bratz_Costa_Dieterich_Felipe/src/gui_app.py
+++ PROVA_PY_GRUPO_Bratz_Costa_Dieterich_Felipe/src/gui_app.py
@@ -1,53 +1,55 @@
 import customtkinter as ctk
 from auth import criar_usuario
+from gui_app2 import abrir_tela_login
 
 ctk.set_appearance_mode("dark")
 
 janela = ctk.CTk()
-janela.title("Login")
-janela.geometry("800x400")
+janela.title("Cadastro")
+janela.geometry("800x450")
 
-titulo = ctk.CTkLabel(janela, text="Para Prosseguir, Faça seu Cadastro!", font=("Arial",22,"bold"))
-titulo.pack(pady=80)
+titulo = ctk.CTkLabel(janela, text="Cadastro de Usuário", font=("Arial",22,"bold"))
+titulo.pack(pady=20)
 
+# -------- Usuário --------
 frame1 = ctk.CTkFrame(janela)
 frame1.pack(pady=10)
 
-t1 = ctk.CTkLabel(frame1, text="Usuário:", font=("Arial",12,"bold"))
-t1.pack(side="left", padx=15)
-
+ctk.CTkLabel(frame1, text="Usuário:", font=("Arial",12,"bold")).pack(side="left", padx=15)
 user = ctk.CTkEntry(frame1, placeholder_text="Digite seu usuário")
 user.pack(side="left")
 
+# -------- Senha --------
 frame2 = ctk.CTkFrame(janela)
-frame2.pack(pady=40)
+frame2.pack(pady=10)
 
-t2 = ctk.CTkLabel(frame2, text="Senha:", font=("Arial",12,"bold"))
-t2.pack(side="left", padx=15)
-
+ctk.CTkLabel(frame2, text="Senha:", font=("Arial",12,"bold")).pack(side="left", padx=15)
 senha = ctk.CTkEntry(frame2, placeholder_text="Digite sua senha", show="*")
 senha.pack(side="left")
 
+# -------- Tipo de conta --------
+frame3 = ctk.CTkFrame(janela)
+frame3.pack(pady=10)
+
+ctk.CTkLabel(frame3, text="Tipo:", font=("Arial",12,"bold")).pack(side="left", padx=15)
+
+# Menu com tipos
+tipos = ["usuario", "operador", "admin"]
+tipo_selecionado = ctk.CTkOptionMenu(frame3, values=tipos)
+tipo_selecionado.pack(side="left")
 
 
-def abrir_tela_principal():
-    nova = ctk.CTk()
-    nova.title("Tela Principal")
-    nova.geometry("800x400")
-
-    label = ctk.CTkLabel(nova, text="Bem-vindo!")
-    label.pack(pady=20)
-
-    nova.mainloop()
-
-
-
+# -------- Lógica --------
 def cadastrar():
-    criar_usuario(user.get(), senha.get(), "usuario")
-    print("Usuário cadastrado!")
+    login = user.get()
+    password = senha.get()
+    tipo = tipo_selecionado.get()
+
+    criar_usuario(login, password, tipo)
+    print(f"Usuário {login} cadastrado como {tipo}!")
 
     janela.destroy()
-    abrir_tela_principal()
+    abrir_tela_login()
 
 
 b = ctk.CTkButton(janela, text="Cadastrar", command=cadastrar)
